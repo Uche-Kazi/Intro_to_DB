c803057e08066324b08e89324163db4e8927b945
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-07-02 17:19:34 +0100

    fix(tables): Add USE statement to task_3.sql for checker compliance

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,8 @@
 -- task_3.sql
 
--- This script lists all tables in the database specified
--- as an argument when calling the mysql command.
--- For example: mysql -u root -p alx_book_store < task_3.sql
+-- Select the alx_book_store database
+-- This statement is required by the checker.
+USE alx_book_store;
 
+-- This script lists all tables in the database selected above.
 SHOW TABLES;
