a4693358c5d7fa3a9f2430651325b274ce0cf5cc
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-07-02 17:11:55 +0100

    feat(tables): Add SQL script to list alx_book_store tables

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- task_3.sql
+
+-- This script lists all tables in the database specified
+-- as an argument when calling the mysql command.
+-- For example: mysql -u root -p alx_book_store < task_3.sql
+
+SHOW TABLES;
